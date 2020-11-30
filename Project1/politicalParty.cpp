bbbb05db41e003a6d29f87363975bb7adc4e2ecf
Or Mocha <or@connecteam.com>
2020-11-30 20:54:03 +0000

    wip

--- Project1/politicalParty.cpp
+++ Project1/politicalParty.cpp
@@ -4,9 +4,28 @@
 
 namespace proj
 {
-	politicalparty::politicalparty() :presentativeList(new citizenList*()), name(nullptr), numId(0),head(nullptr)
+	politicalparty::politicalparty() :representativeList(new citizenList*[]), name(nullptr), numId(0),head(nullptr)
 	{
 
+	}
+
+    void politicalparty::addRepresentitive(citizen* citizen, int state) {
+        if (state > this->lastState){
+            //resize array
+            this->lastState = state;
+        }
+        this->representativeList[state]->add_node(citizen);
+	}
+
+    citizenList* politicalparty::getWinningRepresentitives(int state, int repCount){
+        citizenList* stateRepres = new citizenList[];
+        node* head = this->representativeList[state].head;
+        node* temp = head;
+        for (int i = 0; i < repCount; ++i) {
+            stateRepres->add_node(temp->value)
+            temp = head->next
+        }
+        return stateRepres;
 	}
 	//politicalparty::politicalparty(char* partyName, int IdHead)
 	//{
--- Project1/politicalParty.h
+++ Project1/politicalParty.h
@@ -1,18 +1,19 @@
 #pragma once
+
 #include "citizen.h"
 #include "citizenList.h"
-namespace proj
-{
-	class politicalparty
-	{
-	private:
-		citizenList** presentativeList;
-		char* name;
-		int numId;
-		citizen* head;
-	public:
-		politicalparty();
-		//politicalparty(char* partyName, int IdHead);
 
-	};
+namespace proj {
+    class politicalparty {
+    private:
+        citizenList **representativeList;
+        char *name;
+        int numId;
+        int lastState;
+        citizen *head;
+    public:
+        politicalparty();
+        //politicalparty(char* partyName, int IdHead);
+
+    };
 }
